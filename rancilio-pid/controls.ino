87077b671beb902ac9d99eb54be3b8a4cef1c577
Łukasz Piątkowski <piontec@gmail.com>
2022-09-29 21:53:59 +0200

    preapre declarations

--- rancilio-pid/controls.ino
+++ rancilio-pid/controls.ino
@@ -903,9 +903,9 @@ void hotwaterAction(int state) {
   // Function switches the pump on to dispense hot water
   if (OnlyPID) return;
   if (state == 1) {
-    digitalWrite(pinRelayPumpe, relayON);
+    digitalWrite(pinRelayPumpe, pumpRelayON);
   } else if (state == 0) {
-    digitalWrite(pinRelayPumpe, relayOFF);
+    digitalWrite(pinRelayPumpe, pumpRelayOFF);
   }
   DEBUG_print("running hotwaterAction(%d)\n", state);
   setGpioAction(HOTWATER, state);
