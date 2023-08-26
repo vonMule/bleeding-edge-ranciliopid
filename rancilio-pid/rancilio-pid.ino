6edf88a6508cff24a9cbb9453468725f997e038f
Tobias <21081031+medlor@users.noreply.github.com>
2023-08-26 15:20:50 +0200

    - 3.2.4:   - Many more refactorings by aschoelzhorn. Thanks again!     - Seperated wifi, temperature and other code to dedicated files.     - Introduces rancilio-enums.h file and replaced some defines with enum classes.     - Reorganized OTA (init and handle), OTA code still in "rancilio-pid.ino" because of timer stuff.     - Separated declaration and implementation in some header/code files.   - Part cleanup of includes.   - Some bugfixes and refactorings by Tobias.

--- rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid.ino
@@ -26,7 +26,7 @@
 #include "controls.h"
 #include "PIDBias.h"
 
-const char* sysVersion PROGMEM = "3.2.4 beta2";
+const char* sysVersion PROGMEM = "3.2.4";
 
 /********************************************************
  * definitions below must be changed in the userConfig.h file
